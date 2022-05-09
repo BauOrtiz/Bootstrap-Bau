585be89fce4c2241ab809586633d8252a66ee34a
Bautista Ortiz Aury <bauti707@gmail.com>
2022-05-08 21:50:19 -0300

    Cambios minis

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".nav-menu_visible");
-});
 
